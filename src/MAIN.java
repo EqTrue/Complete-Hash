1450cc4789a4a0a0ea64c82b73fd6e8424be25e1
STEFANDecimelli <decimelli@ibm.com>
2018-05-31 15:34:53 -0400

    Update comments

--- src/MAIN.java
+++ src/MAIN.java
@@ -10,7 +10,7 @@ public class MAIN {
      */
     public static void main(String[] args) {
         char[] key = new char[BIT_LEN];
-        for(int i = 0; i< BIT_LEN; i++) key[i] = '0'; // Initialize
+        for (int i = 0; i < BIT_LEN; i++) key[i] = '0'; // Initialize
 
         /*
         Will continue from 00...0 after ZZ...Z is reached
@@ -26,20 +26,20 @@ public class MAIN {
     Increments value at certain index. Values are from 0-Z, base 36 counting.
      */
     private static char[] increment(char[] key, int index) {
-        key[index] = (char) (++key[index] %91);
+        key[index] = (char) (++key[index] % 91); // Initial Increment
 
-        /*
+        /* CASE 1
         There are 7 characters in between char 0 and A that are not part of the HASH value.
         These values are: : ; < = > ? `
          */
-        if(key[index] == 58) key[index] = (char) (key[index]+ 7);
+        if (key[index] == 58) key[index] = (char) (key[index] + 7);
 
-        /*
+        /* CASE 2
         If the char at index has been looped
          */
-        if(key[index] == 0) {
+        if (key[index] == 0) {
             key[index] = '0';
-            increment(key, (++index)%BIT_LEN); // Increment next bit (RECURSIVE)
+            increment(key, (++index) % BIT_LEN); // Increment next bit (RECURSIVE)
         }
 
         return key; // To be printed
